c93296f5ff1d856fc4cf4c38a41732ee13b0684d
moyongfeng <moyongfeng@live.cn>
2014-10-23 16:40:25 +0800

    oh yeah 2

--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -13,7 +13,6 @@ public:
     static cocos2d::Scene* createScene();
 
     // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
-    virtual bool init();  
     //virtual bool init();  
     
     // implement the "static create()" method manually
