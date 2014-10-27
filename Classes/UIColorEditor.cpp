0c190c400c3239554f12a3c0294d1a3e9c970bb1
moyongfeng <moyongfeng@live.cn>
2014-10-27 12:39:17 +0800

    UI color modify change color array

--- Classes/UIColorEditor.cpp
+++ Classes/UIColorEditor.cpp
@@ -1,13 +1,15 @@
 #include "UIColorEditor.h"
-#if 1
-    #include "UILayer.h"
-#endif
-
 #include "LogicManager.h"
 
 #include "GameUtils.h"
 #include "VisibleRect.h"
 #include "cocos-ext.h"
+
+#define _COLOR_DEBUG_	1
+
+#if _COLOR_DEBUG_
+    #include "UILayer.h"
+#endif
 
 USING_NS_CC;
 USING_NS_CC_EXT;
@@ -16,7 +18,7 @@ USING_NS_CC_EXT;
 #define SPACE_BETWEEN_TWO_BUTTON 5
 #define BUTTON_COLS 10
 #define BUTTON_ROWS 2
-#define BUTTON_NUM 20
+#define BUTTON_NUM BUTTON_COLS*BUTTON_ROWS
 
 UIColorEditor* UIColorEditor::colorEditor = nullptr;
 
@@ -76,10 +78,12 @@ bool UIColorEditor::beginTouchColor(cocos2d::Touch* touch, cocos2d::Event* event
     if (rect.containsPoint(locationInNode)){
         void* p = target->getUserData();
         int index = *(int*)p;
+
+#if _COLOR_DEBUG_
         char num[10];
         sprintf(num, "%d", index);
         UILayer::Layer->addMessage(num);
-        GameLogic::Game->mSelections;
+#endif
 
         if (onSetColorFunc != nullptr)
             onSetColorFunc(mPaletteIndexArray[index], mPaletteColorArray[index]);
@@ -89,8 +93,10 @@ bool UIColorEditor::beginTouchColor(cocos2d::Touch* touch, cocos2d::Event* event
 
 void UIColorEditor::updateColorButtonDisplay(){
     for (int i = 0; i < mColorButtons.size(); i++){
-        if (mPaletteIndexArray[i]>-1 && mColorButtonShow)
-            mColorButtons[i]->setVisible(true);
+		if (mPaletteIndexArray[i]>-1 && mColorButtonShow){
+			mColorButtons[i]->setColor(mPaletteColorArray[i]);
+			mColorButtons[i]->setVisible(true);
+		}
         else
             mColorButtons[i]->setVisible(false);
     }
@@ -103,19 +109,13 @@ void UIColorEditor::initColorButtons(cocos2d::Node* parent){
             r.origin = Point::ZERO;
             r.size = Size(COLOR_BUTTON_SIZE, COLOR_BUTTON_SIZE);
 
-            float colorValue = (i*BUTTON_COLS*1.0+j)/(BUTTON_ROWS * BUTTON_COLS * 1.0f)*255.0f;
-            int colorInt = colorValue;
-            auto button = GameUtils::createRect(r, Color3B(colorInt, colorInt, colorInt));
+            auto button = GameUtils::createRect(r, Color3B(0xFF, 0xFF, 0xFF));
             button->setPosition(Vec2(COLOR_BUTTON_SIZE*(j + 2.5f) + SPACE_BETWEEN_TWO_BUTTON*j, COLOR_BUTTON_SIZE*(2 - i) - SPACE_BETWEEN_TWO_BUTTON*i));
             button->setTexture("images/rect.png");
             void* p = (void*)&indexData[BUTTON_COLS*i + j];
             button->setUserData(p);
             parent->addChild(button);
-
-            mPaletteIndexArray.push_back(i*BUTTON_COLS + j);
-            mPaletteColorArray.push_back(Color3B(colorInt, colorInt, colorInt));
-
-            mColorButtons.push_back(button);
+			mColorButtons.push_back(button);
         }
     }
 
@@ -132,11 +132,55 @@ void UIColorEditor::initColorButtons(cocos2d::Node* parent){
             _eventDispatcher->addEventListenerWithSceneGraphPriority(listener->clone(), mColorButtons[i]);
     }
     
+	cleanColors();
+	addColor(2, Color3B(0xFF, 0xFF, 0xFF));
+	addColor(3, Color3B(0xFF, 0x00, 0x00));
+	addColor(1, Color3B(0x00, 0xFF, 0x00));
+	addColor(4, Color3B(0x00, 0x00, 0xFF));
+
+	addColor(5, Color3B(0xCC, 0xCC, 0xCC));
+	addColor(6, Color3B(0xCC, 0x00, 0x00));
+	addColor(7, Color3B(0x00, 0xCC, 0x00));
+	addColor(8, Color3B(0x00, 0x00, 0xCC));
+
+	addColor(9, Color3B(0x99, 0x99, 0x99));
+	addColor(10, Color3B(0x99, 0x00, 0x00));
+	addColor(11, Color3B(0x00, 0x99, 0x00));
+	addColor(12, Color3B(0x00, 0x00, 0x99));
+
+	addColor(13, Color3B(0x66, 0x66, 0x66));
+	addColor(14, Color3B(0x66, 0x00, 0x00));
+	addColor(15, Color3B(0x00, 0x66, 0x00));
+	addColor(16, Color3B(0x00, 0x00, 0x66));
+
+	addColor(17, Color3B(0x33, 0x33, 0x33));
+	addColor(18, Color3B(0x33, 0x00, 0x00));
+	addColor(19, Color3B(0x00, 0x33, 0x00));
+	addColor(20, Color3B(0x00, 0x00, 0x33));
+
     updateColorButtonDisplay();
 }
 
 void UIColorEditor::cleanColors(){
+	mColorTableEndIndex = 0;
     for (int i = 0; i < mPaletteIndexArray.size(); i++){
         mPaletteIndexArray[i] = -1;
+		mPaletteColorArray[i] = Color3B(0xFF, 0xFF, 0xFF);
     }
+}
+
+void UIColorEditor::addColor(int index, cocos2d::Color3B color){
+	if (mColorTableEndIndex >= BUTTON_NUM) return;
+
+	int size = mPaletteIndexArray.size();
+	if (size - 1 < mColorTableEndIndex){
+		mPaletteIndexArray.push_back(index);
+		mPaletteColorArray.push_back(color);
+	}
+	else {
+		mPaletteIndexArray[mColorTableEndIndex] = index;
+		mPaletteColorArray[mColorTableEndIndex] = color;
+	}
+
+	mColorTableEndIndex++;
 }
--- Classes/UIColorEditor.h
+++ Classes/UIColorEditor.h
@@ -7,12 +7,14 @@ class UIColorEditor{
 public:
     void init(cocos2d::Node* parent);
 
+	void cleanColors();
+	void addColor(int index, cocos2d::Color3B color);
+	void updateColorButtonDisplay();
+
 private:
-    void updateColorButtonDisplay();
     bool beginTouchShowHide(cocos2d::Touch* touch, cocos2d::Event* event);
     bool beginTouchColor(cocos2d::Touch* touch, cocos2d::Event* event);
     void initColorButtons(cocos2d::Node* parent);
-    void cleanColors();
 
 public:
     static UIColorEditor* colorEditor;
@@ -24,7 +26,9 @@ private:
     //std::vector<cocos2d::EventListenerTouchOneByOne*> mEventListners;
 
     std::vector<int> mPaletteIndexArray;
-    std::vector<cocos2d::Color3B> mPaletteColorArray;
+	std::vector<cocos2d::Color3B> mPaletteColorArray;
+
+	int mColorTableEndIndex{ 0 };
 
     bool mColorButtonShow{ true };
 };
